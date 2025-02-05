d3d14910d1bf77cfb16e8090434272691deaeb0d
Dvdrepetto <davidmrepetto@gmail.com>
2025-02-05 20:04:22 +0200

    add Json render in ApartmentList and delete button

--- react-mini-proyect/src/App.jsx
+++ react-mini-proyect/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Footer } from './components/Footer'
 import { Navbar } from './components/Navbar'
 import { Sidebar } from './components/Sidebar'
+import ApartmentsList from './components/apartmentsList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
       <Navbar/>
+      <ApartmentsList/>
       <Sidebar/>
       <Footer/>
     </>
---
+++ react-mini-proyect/src/components/ApartmentsList.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useState } from 'react';
+import apartmentsData from '../Data/Apartments.json'
+import "./ApartmentsList.css";
+
+function ApartmentsList() {
+
+    const [apartmentsToDisplay, setApartments] = useState(apartmentsData);
+
+    const deleteApartment = (id) => {
+        const updatedApartments = apartmentsToDisplay.filter((apartment) => apartment.id !== id);
+        setApartments(updatedApartments);
+    };
+    return (
+        
+        <div className="apartment-list">
+            {apartmentsToDisplay.map((apartment) => {
+                return(
+                    <div key={apartment.id} className="apartment-card">
+                    <img
+                        src={apartment.picture_url}
+                        alt={`Imagen de ${apartment.title}`}
+                        className="apartment-image"
+                    />
+                    <h2 className="apartment-title">{apartment.title}</h2>
+                    <h3>
+                        <strong>Price:</strong> {apartment.price} €
+                    </h3>
+                    <div className="labels">
+                        {apartment.price < 100 && (
+                            <span className="label affordable">💵 Asequible</span>
+                        )}
+                        {apartment.price >= 100 && (
+                            <span className="label premium">💎 Premium</span>
+                        )}
+                        {apartment.instant_bookable && (
+                            <span className="label confirmation">✅ Instant Confirmation</span>
+                        )}
+                    </div>
+                    <button
+                        className="delete-button"
+                        onClick={() => deleteApartment(apartment.id)}
+                    >
+                        Delete
+                    </button>
+                </div>
+                )
+            })}
+        </div>
+    );
+}
+
+export default ApartmentsList;
