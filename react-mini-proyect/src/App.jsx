2c31461c190a8c77abbcb4fd42865fce3a0d58d0
Dvdrepetto <davidmrepetto@gmail.com>
2025-02-10 19:30:12 +0200

    add functionality to add items

--- react-mini-proyect/src/App.jsx
+++ react-mini-proyect/src/App.jsx
@@ -11,16 +11,43 @@ import About from './pages/About';
 
 function App() {
 
-  const [apartmentsToDisplay, setApartments] = useState(apartmentsData);
+const [apartmentsToDisplay, setApartments] = useState(apartmentsData);
+const[name, setName] = useState("");
 
   const deleteApartment = (id) => {
     const updatedApartments = apartmentsToDisplay.filter((apartment) => apartment.id !== id);
     setApartments(updatedApartments);
 };
 
+
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const newApartment = {
+      name: name,
+  }
+
+  const newList = [newApartment, ...apartmentsToDisplay];
+
+  console.log(newList)
+  setApartments(newList);
+}
   return (
     <>
-      <Navbar/>
+      <Navbar numberOfApartments={apartmentsToDisplay.length} />
+      <section className='form'>
+        <form onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          name='name' 
+          placeholder='Apartments'
+          value={name}
+          onChange={(e) => { setName(e.target.value) }}
+          />
+          <button>Submit</button>
+        </form>
+      </section>
       <Routes>
       <Route path="/" element={<ApartmentsList apartmentArr={apartmentsToDisplay} callBackToDelete={deleteApartment}/>}/>
       <Route path="/ApartmentsCard/:apartmentId" element={<ApartmentCard apartmentArr={apartmentsToDisplay}/>}/>
--- react-mini-proyect/src/components/Navbar.jsx
+++ react-mini-proyect/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import{ NavLink } from "react-router-dom";
 
-function Navbar () {
+function Navbar (props) {
     return (
         <header>
-            <h1>This is the Navbar</h1>
-            
         <nav>
                 <NavLink to="/">Home</NavLink> |
                 <NavLink to="ApartmentsCard">Details</NavLink> |
                 <NavLink to="/About">About us</NavLink> |
         </nav>
+        <h1>This is the Navbar</h1>
+            <h2>Number of movies: {props.numberOfApartments}</h2>
         </header>
     )
 }
